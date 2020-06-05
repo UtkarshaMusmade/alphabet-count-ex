0902a697e72fbb227ebd3286879eb65286940577
UtkarshaMusmade <utkarsha.musmade@example.com>
2020-06-05 14:51:40 +0530

    Updated

--- src/main/scala/AlphabetCount.scala
+++ src/main/scala/AlphabetCount.scala
@@ -2,20 +2,25 @@ object AlphabetCount extends App {
   val string = "Knol is a unit of Knowledge & Dus comes from Druksh which is Sanskrit for a tree, hence Knoldus is a tree of Knowledge."
 
   println(characterCount(string))
-println(alphabetCount(string))
+  println(wordContainAlphabates(string, 2))
+
   private def characterCount(string: String) = {
-    val list = string.replace(" ", "").toList
-   list.map {
+    val list = string.replace(" ", "").toLowerCase.toList
+
+    list.map {
       char =>
         val count = list.count(_ == char)
         Map(char -> count)
-    }
+    }.distinct.flatten
+
   }
 
-  def alphabetCount(string: String)={
-   val list = string.split(" ").toList
-    list.map{
-      word =>word
+  def wordContainAlphabates(string: String, n: Int) = {
+    val alphabetOccurence = characterCount(string)
+    val mapOfNOccurences = alphabetOccurence.filter {
+      keyValue => keyValue._2 == n
     }
+    mapOfNOccurences.map { alphabets => alphabets._1
+    }.filter(char => char.isLetter).mkString
   }
 }
